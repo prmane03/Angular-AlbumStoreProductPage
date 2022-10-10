12f97a7d8b602d6bcc752b985b950322e9df7660
PratikR Mane <PratikR.Mane@lntinfotech.com>
2022-10-10 11:09:17 +0530

    created Interfaces Album, Track | added proper types in ProductService and PorductDescriptionComponent

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,8 +13,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
   
-  getAlbum(id: number){
+  getAlbum(id: number) : Observable<Album>{
     return this._http.get(this._albumUrl)
-    .map(response => response.json());
+    .map(response =>  <Album> response.json());
   }
 }
